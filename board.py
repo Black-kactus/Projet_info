6f05ac15f07c434e86b5bafa25b84f0abd69a157
Black-kactus <59531651+Black-kactus@users.noreply.github.com>
2021-11-05 11:50:03 +0100

    Create board.py

---
+++ board.py
@@ -0,0 +1,8 @@
+#echiquier
+
+#cooordonées de chaques cases
+#possibilité d'avoir une seule case
+#règles du jeu d'échec
+#savoir si une pièces est attaquée
+
+#attribut: quel camp joue/droit au roque
