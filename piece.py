b929bca8e7ccffc766bc8db92fafc1704b56c02b
Raphael-Renard <93404597+Raphael-Renard@users.noreply.github.com>
2021-11-10 17:26:47 +0100

    Update piece.py

--- piece.py
+++ piece.py
@@ -4,12 +4,44 @@ class piece():
     def __init__ (self,couleur,colonne,ligne,numero,valeur=3):
       self._couleur=couleur
       self._valeur=valeur
-      self.
+      self.ligne=ligne
+      self.colonne=colonne
+      self.numero=numero
   class roi(piece):
+     def __init__ (self,couleur,colonne,ligne,numero,valeur=0):
+      self._couleur=couleur
+      self._valeur=valeur
+      self.ligne=ligne
+      self.colonne=colonne
+      self.numero=numero
   class dame (piece):
+     def __init__ (self,couleur,colonne,ligne,numero,valeur=9):
+      self._couleur=couleur
+      self._valeur=valeur
+      self.ligne=ligne
+      self.colonne=colonne
+      self.numero=numero
   class cavalier (piece):
+     def __init__ (self,couleur,colonne,ligne,numero,valeur=3):
+      self._couleur=couleur
+      self._valeur=valeur
+      self.ligne=ligne
+      self.colonne=colonne
+      self.numero=numero
   class tour (piece):
+     def __init__ (self,couleur,colonne,ligne,numero,valeur=5):
+      self._couleur=couleur
+      self._valeur=valeur
+      self.ligne=ligne
+      self.colonne=colonne
+      self.numero=numero
   class pion (piece):
+     def __init__ (self,couleur,colonne,ligne,numero,valeur=1):
+      self._couleur=couleur
+      self._valeur=valeur
+      self.ligne=ligne
+      self.colonne=colonne
+      self.numero=numero
   #attributs de chaque piece :
     #couleur
     #coordonées
