338ee8c309e189a88ba493765565c77251143aa4
Raphael-Renard <93404597+Raphael-Renard@users.noreply.github.com>
2021-11-10 22:00:19 +0100

    Update piece.py

--- piece.py
+++ piece.py
@@ -22,6 +22,12 @@ class piece():
     return nom
     #on a un nom du type "PN3" pour le pion noir 3
 
+	
+#attributs de chaque piece :
+  #couleur
+  #coordonées sur le board
+  #valeur
+  #numéro de piece (ex : 1 pour le pion 1 ou le cavalier 1..)
 class fou(piece):
   def __init__ (self,couleur,colonne,ligne,numero,valeur=3):
     self._couleur=couleur
@@ -29,6 +35,7 @@ class fou(piece):
     self.ligne=ligne
     self.colonne=colonne
     self._numero=numero
+
 class roi(piece):
   def __init__ (self,couleur,colonne,ligne,numero,valeur=0):
     self._couleur=couleur
@@ -36,6 +43,7 @@ class roi(piece):
     self.ligne=ligne
     self.colonne=colonne
     self._numero=numero
+
 class dame (piece):
   def __init__ (self,couleur,colonne,ligne,numero,valeur=9):
     self._couleur=couleur
@@ -43,6 +51,7 @@ class dame (piece):
     self.ligne=ligne
     self.colonne=colonne
     self._numero=numero
+
 class cavalier (piece):
   def __init__ (self,couleur,colonne,ligne,numero,valeur=3):
     self._couleur=couleur
@@ -50,13 +59,7 @@ class cavalier (piece):
     self.ligne=ligne
     self.colonne=colonne
     self._numero=numero
-class tour (piece):
-  def __init__ (self,couleur,colonne,ligne,numero,valeur=5):
-    self._couleur=couleur
-    self._valeur=valeur
-    self.ligne=ligne
-    self.colonne=colonne
-    self._numero=numero
+
 class pion (piece):
   def __init__ (self,couleur,colonne,ligne,numero,valeur=1):
     self._couleur=couleur
@@ -64,11 +67,26 @@ class pion (piece):
     self.ligne=ligne
     self.colonne=colonne
     self._numero=numero
-#attributs de chaque piece :
-  #couleur
-  #coordonées sur le board
-  #valeur
-  #numéro de piece (ex : 1 pour le pion 1 ou le cavalier 1..)
+
+class tour (piece):
+  def __init__ (self,couleur,colonne,ligne,numero,valeur=5):
+    self._couleur=couleur
+    self._valeur=valeur
+    self.ligne=ligne
+    self.colonne=colonne
+    self._numero=numero
+  def mouvementpossible(self,case):
+    ligne=case[1]
+    colonne=case[0]
+    if ((ligne != self.ligne) and (colonne != self.colonne)) or ligne>7 or colonne>7 or ligne<0 or colonne<0: #pas le bon "motif" de déplacement ou sortie de l'échiquier
+	return None
+    if ligne=self.ligne: #si la tour bouge en horizontale
+	if colonne > self.colonne:
+	   for c in range(self.colonne,colonne):
+		
+
+
+
 
   
 
