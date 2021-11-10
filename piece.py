47a7353aa7af4aa6cb83dd5fe18de1f5ea7e1bae
Raphael-Renard <93404597+Raphael-Renard@users.noreply.github.com>
2021-11-10 17:13:07 +0100

    Update piece.py

--- piece.py
+++ piece.py
@@ -1,1 +1,13 @@
 #OPP pour instancier les différentes pieces
+class piece():
+  class fou(piece):
+  class roi(piece):
+  class dame (piece):
+  class cavalier (piece):
+  class tour (piece):
+  class pion (piece)
+  #attributs de chaque piece :
+    #couleur
+    #coordonées
+    #valeur
+    #numéro de piece (ex : 1 pour le pion 1 ou le cavalier 1..)
