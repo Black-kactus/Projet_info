46eb276bed00bbfe769ed7215f3d5a26a144b1ee
Lila <lila.atifi@orange.fr>
2021-11-10 15:39:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,9 +18,16 @@ piece = Echec()
 
 print(piece.valeur)
 
+<<<<<<< Updated upstream
 print("autre modification")
 
 
 ## à ne pas modifier
 
 print("Bonjour,il y a un problème")
+=======
+piece.valeur +=1
+
+print(piece.valeur)
+## à ne pas modifier
+>>>>>>> Stashed changes
