048a52fe37c0ecf1ad6ad04d32194cee482bfa12
Raphael-Renard <93404597+Raphael-Renard@users.noreply.github.com>
2021-11-10 17:35:52 +0100

    Update board.py

--- board.py
+++ board.py
@@ -6,3 +6,6 @@
 #savoir si une pièces est attaquée
 
 #attribut: quel camp joue/droit au roque
+
+L=[[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8]]
+#echiquier en liste de listes L[0][5] pour dire qu'on ait en A6 par exemple
