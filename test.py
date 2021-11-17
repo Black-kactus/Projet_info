2158a8b0f70dc9530d07bbd5eb1659f6c03a62d4
Lila Atifi <lila.atifi@gmail.com>
2021-11-17 15:13:45 +0100

    Modification interface
    Ajout de boutons, entry et labels actualisé, ajout piece vide

--- test.py
+++ test.py
@@ -11,27 +11,16 @@ content = ttk.Frame(root, padding=(0,0,0,0))
 frame = ttk.Frame(content, borderwidth=0, relief="ridge", width=100, height=100)
 
 
-# myimg = ImageTk.PhotoImage(Image.open('fou_noir.png'))
+# Variables de Tkinter
+deplacement = StringVar()
+deplacement.set("E3")
 
+coup = StringVar()
+coup.set("fouN2")
 
+couleurA = StringVar()
+couleurA.set("blanc")
 
-
-# namelbl = ttk.Label(content, text="Name")
-# name = ttk.Entry(content)
-
-# onevar = BooleanVar()
-# twovar = BooleanVar()
-# threevar = BooleanVar()
-
-# onevar.set(True)
-# twovar.set(False)
-# threevar.set(True)
-
-# one = ttk.Checkbutton(content, text="One", variable=onevar, onvalue=True)
-# two = ttk.Checkbutton(content, text="Two", variable=twovar, onvalue=True)
-# three = ttk.Checkbutton(content, text="Three", variable=threevar, onvalue=True)
-# ok = ttk.Button(content, text="Valider")
-# cancel = ttk.Button(content, text="Abandonner")
 
 content.grid(column=0, row=0, sticky=(N, S, E, W))
 
@@ -68,30 +57,23 @@ for i in range(2,18,2):
     Listepiece.append(sousListe)
         #creation de la liste contenant les 64 pièces d'éches numérotés de 0 à 63
 
-#T,C,F,Q,K,P = tour, cavalier, fou, reine(queen), roi(king), pion #N,B = noir, blanc position=[[TB1,CB1,FB1,QB1,KB1,FB2,CB2,TB2],[PB1,PB2,PB3,PB4,PB5,PB6,PB7,PB8],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[PN1,PN2,PN3,PN4,PN5,PN6,PN7,PN8],[TN1,CN1,FN1,QN1,KN1,FN2,CN2,TN2]]
 
-# for i in range(len(Listepiece)):
-#    Listepiece[i].grid(column=L[i][0], row=L[i][1], columnspan=2, rowspan=2,sticky=(N,S,E,W),pady=1, padx=1)
-
-dicopiece = {}
+dicopiece = {"":"vide.png"}
 
 dicopieceB = {"TB1":"tour_blanche.png","CB1":"cavalier_blanc.png","FB1":"fou_blanc.png","QB1":"reine_blanche.png","KB1":"","FB2":"fou_blanc.png","CB2":"cavalier_blanc.png","TB2":"tour_blanche.png"}
 dicopieceN= {"TN1":"tour_noire.png","CN1":"cavalier_noir.png","FN1":"fou_noir.png","QN1":"reine_noire.png","KN1":"","FN2":"fou_noir.png","CN2":"cavalier_noir.png","TN2":"tour_noire.png"}
 dicopiecepionB = {"PB1":"pion_blanc.png","PB2":"pion_blanc.png","PB3":"pion_blanc.png","PB4":"pion_blanc.png","PB5":"pion_blanc.png","PB6":"pion_blanc.png","PB7":"pion_blanc.png","PB8":"pion_blanc.png"}
 dicopiecepionN = {"PN1":"pion_noir.png","PN2":"pion_noir.png","PN3":"pion_noir.png","PN4":"pion_noir.png","PN5":"pion_noir.png","PN6":"pion_noir.png","PN7":"pion_noir.png","PN8":"pion_noir.png"}
 
-
-dicopiece= dict(dicopieceB)
 dicopiece.update(dicopieceN)
+dicopiece.update(dicopieceB)
 dicopiece.update(dicopiecepionB)
 dicopiece.update(dicopiecepionN)
 
-print(dicopiece["TN1"])
 
-
-img2 = PhotoImage(file = dicopiece["FB1"])
+img2 = PhotoImage(file = dicopiece[""])
 img2 = img2.subsample(15, 15)
-Listepiece[2][0] = ttk.Label(content, text= str(i)+","+str(j),relief="solid",image = img2,anchor=CENTER, background= "red")
+Listepiece[2][0] = ttk.Label(content, text= str(i)+","+str(j),relief="solid",image = img2,anchor=CENTER,background = "red")
 
 
 def afficherPiece():
@@ -99,7 +81,24 @@ def afficherPiece():
         for j in range(8):
             Listepiece[j][i].grid(column=2+2*j, row=2+2*i, columnspan=2, rowspan=2,sticky=(N,S,E,W),pady=1, padx=1)
 
-afficherPiece()
+def cmd_bouton_valider():
+    print("valider")
+    if couleurA.get() == "blanc":
+        couleurA.set("noir")
+        print("noir")
+    else:
+        couleurA.set("blanc")
+        print("blanc")
+    # afficherPiece()
+
+def cmd_bouton_commencer():
+    afficherPiece()
+    print("commencer")
+
+def cmd_bouton_abandonner():
+    print("abandonner")
+
+# afficherPiece()
 
 
 root.columnconfigure(0, weight=1)
@@ -116,34 +115,39 @@ largeur = 4
 Lvide = ttk.Label(content, text= "",relief="solid",anchor=CENTER)
 Lvide.grid(column=18, row=0, columnspan=2, rowspan=18,sticky=(N,S,E,W),pady=1, padx=1)
 
-Lcommencer = ttk.Label(content, text= "Commencer",relief="solid",anchor=CENTER)
-Lcommencer.grid(column=20, row=0, columnspan=largeur, rowspan=2,sticky=(N,S,E,W),pady=1, padx=1)
+Bouton_commencer = ttk.Button(content, text= "Commencer",command= cmd_bouton_commencer)
+Bouton_commencer.grid(column=20, row=0, columnspan=largeur, rowspan=2,sticky=(N,S,E,W),pady=1, padx=1)
 
-Lcouleurquijoue = ttk.Label(content, text= "Couleur qui joue",relief="solid",anchor=CENTER)
-Lcouleurquijoue.grid(column=20, row=2, columnspan=largeur, rowspan=1,sticky=(N,S,E,W),pady=1, padx=1)
+Label_couleurquijoue = ttk.Label(content, text= "Couleur qui joue",relief="solid",anchor=CENTER)
+Label_couleurquijoue.grid(column=20, row=2, columnspan=largeur, rowspan=1,sticky=(N,S,E,W),pady=1, padx=1)
 
-Lcouleuractualise= ttk.Label(content, text= "blanc",relief="solid",anchor=CENTER)
-Lcouleuractualise.grid(column=20,row=3, columnspan=largeur, rowspan=1,sticky=(N,S,E,W),pady=1, padx=1)
+Entry_couleuractualise= ttk.Entry(content, textvariable= couleurA)
+Entry_couleuractualise.grid(column=20,row=3, columnspan=largeur, rowspan=1,sticky=(N,S,E,W),pady=1, padx=1)
 
 Lpieceabouger = ttk.Label(content, text= "Piece à bouger",relief="solid",anchor=CENTER)
 Lpieceabouger.grid(column=20, row=4, columnspan=largeur, rowspan=1,sticky=(N,S,E,W),pady=1, padx=1)
 
-Lpieceactualise= ttk.Label(content, text= "fou2N",relief="solid",anchor=CENTER)
-Lpieceactualise.grid(column=20,row=5, columnspan=largeur, rowspan=1,sticky=(N,S,E,W),pady=1, padx=1)
+Entry_deplacement= ttk.Entry(content, textvariable= deplacement)
+Entry_deplacement.grid(column=20,row=5, columnspan=largeur, rowspan=1,sticky=(N,S,E,W),pady=1, padx=1)
 
-Lcoupajouer = ttk.Label(content, text= "Coup à jouer",relief="solid",anchor=CENTER)
-Lcoupajouer.grid(column=20, row=6, columnspan=largeur, rowspan=1,sticky=(N,S,E,W),pady=1, padx=1)
+Label_coupajouer = ttk.Label(content, text= "Coup à jouer",relief="solid",anchor=CENTER)
+Label_coupajouer.grid(column=20, row=6, columnspan=largeur, rowspan=1,sticky=(N,S,E,W),pady=1, padx=1)
 
-Lcoupactualise= ttk.Label(content, text= "E3",relief="solid",anchor=CENTER)
-Lcoupactualise.grid(column=20,row=7, columnspan=largeur, rowspan=1,sticky=(N,S,E,W),pady=1, padx=1)
+Entry_coup= ttk.Entry(content, textvariable= coup)
+Entry_coup.grid(column=20,row=7, columnspan=largeur, rowspan=1,sticky=(N,S,E,W),pady=1, padx=1)
 
-Lvalider= ttk.Label(content, text= "Valider",relief="solid",anchor=CENTER)
-Lvalider.grid(column=20,row=8, columnspan=largeur, rowspan=2,sticky=(N,S,E,W),pady=1, padx=1)
+Bouton_valider= ttk.Button(content, text= "Valider",command= cmd_bouton_valider)
+Bouton_valider.grid(column=20,row=8, columnspan=largeur, rowspan=2,sticky=(N,S,E,W),pady=1, padx=1)
 
-Labandonner= ttk.Label(content, text= "Abandonner",relief="solid",anchor=CENTER)
-Labandonner.grid(column=20,row=16, columnspan=largeur, rowspan=2,sticky=(N,S,E,W),pady=1, padx=1)
+Bouton_abandonner= ttk.Button(content, text= "Abandonner",command = cmd_bouton_abandonner)
+Bouton_abandonner.grid(column=20,row=16, columnspan=largeur, rowspan=2,sticky=(N,S,E,W),pady=1, padx=1)
 
 
+
+
+
+
+##################
 # L1= ttk.Label(content, text= "test1",relief="solid")
 # L1.grid(column=20,row=10, columnspan=largeur, rowspan=1,sticky=(N,S,E,W),pady=1, padx=1)
 
@@ -186,6 +190,24 @@ Labandonner.grid(column=20,row=16, columnspan=largeur, rowspan=2,sticky=(N,S,E,W
 # La = Label(root, image = img)
 # La.grid(column = 10, row = 10, columnspan = 2, rowspan = 2)
 
+# myimg = ImageTk.PhotoImage(Image.open('fou_noir.png'))
 
+
+# namelbl = ttk.Label(content, text="Name")
+# name = ttk.Entry(content)
+
+# onevar = BooleanVar()
+# twovar = BooleanVar()
+# threevar = BooleanVar()
+
+# onevar.set(True)
+# twovar.set(False)
+# threevar.set(True)
+
+# one = ttk.Checkbutton(content, text="One", variable=onevar, onvalue=True)
+# two = ttk.Checkbutton(content, text="Two", variable=twovar, onvalue=True)
+# three = ttk.Checkbutton(content, text="Three", variable=threevar, onvalue=True)
+# ok = ttk.Button(content, text="Valider")
+# cancel = ttk.Button(content, text="Abandonner")
 
 root.mainloop()
