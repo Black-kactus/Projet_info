b8659085499a08131b3cdd82c1322f3af02b38bd
Raphael-Renard <raphaelrenard2004@gmail.com>
2021-12-01 15:22:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-
 from test import coup,deplacement
+from board import mouvement,position
+
 def interpreteur(coup,deplacement):
     coup=coup.get()
     deplacement=deplacement.get()
-    a=ord(coup[1])-97
+    colonne_P=ord(coup[0])-97 #position de la pièce à bouger
+    ligne_P=coup[1]-1 #position de la pièce à bouger
+    colonne_A=ord(deplacement[0])-97 #position de la case d'arrivée
+    ligne_A=deplacement[1]-1 #position de la case d'arrivée
+    mouvement(position[colonne_P][ligne_P],[colonne_A,ligne_A]) #case = liste des 2 coordonées de la case : [colonne,ligne]
+    
+
 
