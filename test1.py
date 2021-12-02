a979a983d91af5fcea9da1dec23425a2518600e4
WhiteLoup <andral.lou@gmail.com>
2021-12-02 12:16:41 +0100

    Changement sur piece.py
    Version Final Mvmt Pion, vérifié

--- test1.py
+++ test1.py
@@ -1,1 +1,8 @@
 #ce fichier est ouvert à tout le monde pour faire des tests
+
+
+#test des fonctions mouvement piece
+
+#position = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,PB1,0,0,0,0,0],[0,PB2,PB3,PB4,0,0,0,0],[0,0,PB5,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]]
+#P = pion("Blanc",3,1,2)
+#print(pion.mouvementpossible(P,5,6))
