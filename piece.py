cd09bd7dc9b5576eb0ca044c35bd3e43be4f10f0
Raphael-Renard <raphaelrenard2004@gmail.com>
2021-12-04 15:38:48 +0100

    Update piece.py

--- piece.py
+++ piece.py
@@ -50,37 +50,30 @@ class Fou(Piece):
             K = 0
             cpt = 0
             for k in range(-8, 9):  # motif de déplacement du fou
-                if (colonne == self.colonne + k and ligne == self.ligne + k) or (
-                        colonne == self.colonne - k and ligne == self.ligne + k):
+                if (colonne == self.colonne + k and ligne == self.ligne + k) or (colonne == self.colonne - k and ligne == self.ligne + k):
                     cpt += 1
                     K = k
             if cpt != 1:  # pas le bon "motif" de déplacement
                 return False
 
-            elif (
-                    colonne == self.colonne + K and ligne == self.ligne + K):  # si le fou bouge sur la diagonale de bas gauche à haut droit (/)
+            elif (colonne == self.colonne + K and ligne == self.ligne + K):  # si le fou bouge sur la diagonale de bas gauche à haut droit (/)
                 if K > 0:  # si le fou va vers le haut gauche
                     for k in range(1, K):
-                        if position[self.colonne + k][
-                            self.ligne + k] != 0:  # s'il y a une autre piece en travers du chemin
+                        if position[self.colonne + k][self.ligne + k] != 0:  # s'il y a une autre piece en travers du chemin
                             return False
                 else:  # si le fou va vers le bas droit
                     for k in range(1, -(K - 1)):
-                        if position[self.colonne - k][
-                            self.ligne - k] != 0:  # s'il y a une autre piece en travers du chemin
+                        if position[self.colonne - k][self.ligne - k] != 0:  # s'il y a une autre piece en travers du chemin
                             return False
 
-            elif (
-                    colonne == self.colonne - K and ligne == self.ligne + K):  # si le fou bouge sur la diagonale de haut gauche à bas droit (\)
+            elif (colonne == self.colonne - K and ligne == self.ligne + K):  # si le fou bouge sur la diagonale de haut gauche à bas droit (\)
                 if K > 0:  # si le fou va vers le haut droit
                     for k in range(1, K):
-                        if position[self.colonne - k][
-                            self.ligne + k] != 0:  # s'il y a une autre piece en travers du chemin
+                        if position[self.colonne - k][self.ligne + k] != 0:  # s'il y a une autre piece en travers du chemin
                             return False
                 else:  # si la tour va vers le bas gauche
                     for k in range(1, -(K - 1)):
-                        if position[self.colonne + k][
-                            self.ligne - k] != 0:  # s'il y a une autre piece en travers du chemin
+                        if position[self.colonne + k][self.ligne - k] != 0:  # s'il y a une autre piece en travers du chemin
                             return False
         return True
 
@@ -92,7 +85,10 @@ class Roi(Piece):
         self.ligne = ligne
         self.colonne = colonne
         self._numero = numero
-        self.echec=echec
+        self._echec=echec
+
+    def Echec(self): ##??
+        return True
 
 
 class Dame(Piece):
@@ -116,14 +112,7 @@ class Cavalier(Piece):
         from board import position
         if colonne == self.colonne and ligne == self.ligne:  # si le cavalier ne bouge pas
             return True
-        elif ((colonne != self.colonne - 2 and ligne != self.ligne + 1) or (
-                colonne != self.colonne - 2 and ligne != self.ligne - 1) or (
-                      colonne != self.colonne - 1 and ligne != self.ligne + 2) or (
-                      colonne != self.colonne - 1 and ligne != self.ligne - 2) or (
-                      colonne != self.colonne + 1 and ligne != self.ligne + 2) or (
-                      colonne != self.colonne + 1 and ligne != self.ligne - 2) or (
-                      colonne != self.colonne + 2 and ligne != self.ligne + 1) or (
-                      colonne != self.colonne + 2 and ligne != self.ligne - 1)) or ligne > 7 or colonne > 7 or ligne < 0 or colonne < 0:  # pas le bon "motif" de déplacement ou sortie de l'échiquier
+        elif ((colonne != self.colonne - 2 and ligne != self.ligne + 1) or (colonne != self.colonne - 2 and ligne != self.ligne - 1) or (colonne != self.colonne - 1 and ligne != self.ligne + 2) or (colonne != self.colonne - 1 and ligne != self.ligne - 2) or (colonne != self.colonne + 1 and ligne != self.ligne + 2) or (colonne != self.colonne + 1 and ligne != self.ligne - 2) or (colonne != self.colonne + 2 and ligne != self.ligne + 1) or (colonne != self.colonne + 2 and ligne != self.ligne - 1)) or ligne > 7 or colonne > 7 or ligne < 0 or colonne < 0:  # pas le bon "motif" de déplacement ou sortie de l'échiquier
             return False
         else:
             return True
@@ -153,16 +142,14 @@ class Pion(Piece):
                         return True
                 if ligne == (self.ligne + 2) and self._couleur == "Blanc":
                     if self.Move1:  # premier mouvement du pion ?
-                        if position[colonne][ligne - 1] == 0 and position[colonne][
-                            ligne] == 0:  # s'il n'y a pas de piece sur la case d'arrivée
+                        if position[colonne][ligne - 1] == 0 and position[colonne][ligne] == 0:  # s'il n'y a pas de piece sur la case d'arrivée
                             return True
                 if ligne == (self.ligne - 1) and self._couleur != "Blanc":
                     if position[colonne][ligne] == 0:  # s'il n'y a pas de piece sur la case d'arrivée
                         return True
                 if ligne == (self.ligne - 2) and self._couleur != "Blanc":
                     if self.Move1:  # premier mouvement du pion ?
-                        if position[colonne][ligne + 1] == 0 and position[colonne][
-                            ligne] == 0:  # s'il n'y a pas de piece sur la case d'arrivée
+                        if position[colonne][ligne + 1] == 0 and position[colonne][ligne] == 0:  # s'il n'y a pas de piece sur la case d'arrivée
                             return True
                 # else:
                 # return False
@@ -187,8 +174,7 @@ class Tour(Piece):
 
     def mouvement_possible(self, colonne, ligne):  # indique si la tour peut bouger jusqu'à la case indiquée
         from board import position
-        if ((ligne != self.ligne) and (
-                colonne != self.colonne)) or ligne > 7 or colonne > 7 or ligne < 0 or colonne < 0:  # pas le bon "motif" de déplacement ou sortie de l'échiquier
+        if ((ligne != self.ligne) and (colonne != self.colonne)) or ligne > 7 or colonne > 7 or ligne < 0 or colonne < 0:  # pas le bon "motif" de déplacement ou sortie de l'échiquier
             return False
         elif colonne == self.colonne and ligne == self.ligne:  # si la tour ne bouge pas
             return True
