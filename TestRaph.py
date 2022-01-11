0fb169f967f6039585bf27791ccef7c2084402d6
Raphael-Renard <93404597+Raphael-Renard@users.noreply.github.com>
2022-01-11 20:49:11 +0100

    Delete TestRaph.py

--- TestRaph.py
+++
@@ -1,8 +0,0 @@
-#test
-from piece import Piece,Fou,Tour,Cavalier,Dame,Roi,Pion,promoDameB
-from board import position
-#promoDameB(position[0][1])
-#print(position)
-
-for c in range(2,11):
-    print(f"DB{c}")
